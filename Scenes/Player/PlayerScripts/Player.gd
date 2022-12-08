f7401390fe6d29c839365c068d12c44ec658e07a
E3BB <78990896+E3BB@users.noreply.github.com>
2022-12-08 12:14:25 -0500

    Made testing sprite, made player drift, made quick velocity display

--- Scenes/Player/PlayerScripts/Player.gd
+++ Scenes/Player/PlayerScripts/Player.gd
@@ -1,11 +1,24 @@
 extends KinematicBody2D
 
 # Variables
-
+var vel : Vector2 # Velocity
+var inputLR : float # Left/Right input, expressed from -1 to 1
 
 # Physics Process Run
 func _physics_process(delta):
 	
-	#umm
-	pass
+	# Set Input
+	inputLR = (Input.get_action_strength("Right") - Input.get_action_strength("Left")) * 96
+	
+	# Velocity Check and etc.
+	vel.x += inputLR * delta
+	
+	vel.x = vel.x if vel.x < 64 else 64
+	vel.x = vel.x if vel.x > -64 else -64
+	
+	# Move
+	move_and_slide(vel, Vector2(0, -1))
+	
+	# Update Speed Label
+	$"Camera2D/Label".text = "Vel.x = " + String(vel.x)
 	
