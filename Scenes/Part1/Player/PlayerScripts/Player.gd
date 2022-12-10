7839fabb3438a30333bf60692a97880249cbbc80
E3BB <78990896+E3BB@users.noreply.github.com>
2022-12-09 22:51:03 -0500

    Made Pt1 BG, added ODS, added Drag, minor changes
    + means Added, - means Removed, = means Reworked/Changed.
    +Pt1 BG, +ODS, +Drag, =Jump Conditions, -Tileset

--- Scenes/Part1/Player/PlayerScripts/Player.gd
+++ Scenes/Part1/Player/PlayerScripts/Player.gd
@@ -3,32 +3,39 @@ extends KinematicBody2D
 # Variables
 var vel : Vector2 # Velocity
 var inputLR : float # Left/Right input, expressed from -1 to 1
-var acc : float = 96 # Player Acceleration
-var maxspeed : int = 96 # Maximum speed
-var gravity : float = 9.8 * 16 # Gravity
-var jump_strength : float = 64 # Jump Strength (How much speed you get when you jump)
+var acc : float = 128 # Player Acceleration
+var gravity : float = 30 * 16 # Gravity
+var jump_strength : float = 256 # Jump Strength (How much speed you get when you jump)
+var drag : float = 256 # Drag force, measured in pixels per second per second
+var stopPower : float = 512 # Power of Stopping (When you move a different way than velocity)
 
 # Physics Process Run
 func _physics_process(delta):
 	
 	# Set Input
-	inputLR = (Input.get_action_strength("Right") - Input.get_action_strength("Left")) * acc
+	inputLR = (Input.get_action_strength("Right") - Input.get_action_strength("Left"))
 	
 	# Gravity
 	vel.y += gravity * delta
 	vel.y = 0 if is_on_floor() else vel.y
 	
-	# Velocity Check and etc.
+	# Velocity Check for Walls
 	vel.x = 0 if is_on_wall() else vel.x
 	
-	vel.x = vel.x if vel.x < maxspeed else maxspeed
-	vel.x = vel.x if vel.x > -maxspeed else -maxspeed
+	# Drag Calculations & Opposite-Direction-Speed
+	vel.x -= drag * delta if inputLR == 0 && vel.x > 0 and is_on_floor() else 0
+	vel.x += drag * delta if inputLR == 0 && vel.x < 0 and is_on_floor() else 0
+	vel.x = 0 if vel.x < drag * delta && vel.x > 0 && is_on_floor() && inputLR == 0 else vel.x
+	vel.x = 0 if vel.x > drag * delta && vel.x < 0 && is_on_floor() && inputLR == 0 else vel.x
+	
+	vel.x -= abs(inputLR) * stopPower * delta if inputLR < 0 && vel.x > 0 else 0
+	vel.x -= -abs(inputLR) * stopPower * delta if inputLR > 0 && vel.x < 0 else 0
 	
 	# Left-Right Input
-	vel.x += inputLR * delta
+	vel.x += inputLR * acc * delta
 	
 	# Jump
-	if Input.is_action_just_pressed("Jump"):
+	if Input.is_action_pressed("Jump") && is_on_floor():
 		vel.y -= jump_strength
 	
 	# Move
